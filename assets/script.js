dc6616bca766141e7d077eac050668a2c6bcad12
JamesAChavesForero <javeschames2024@gmail.com>
2024-08-02 13:40:37 -0500

    fetch swap

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,17 @@
 console.log("script running",document.querySelector('#formContainer form'))
-document.querySelector('#formContainer form').addEventListener("submit",(e)=>{
+document.querySelector('.btn').addEventListener("click",()=>{
+    console.log("submitted")
+// setTimeout(window.location.reload("https://cevi-med-dev.github.io/Call-Log-Form/"), 2000);
+})
+
+let form = document.querySelector('#formContainer form')
+
+form.addEventListener('submit',(e)=>{
     e.preventDefault()
+    const fn = new FormData(form);
+    let staObject = Object.fromEntries(fn)
+    console.log("this is the data retreived",fn )
+    // for(item of fn){
+    //     console.log(item)
+    // }
 })
