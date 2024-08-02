34b6c09d9c00b52ca22962de70effedce72e9697
JamesAChavesForero <javeschames2024@gmail.com>
2024-08-02 14:10:36 -0500

    .

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,31 @@
-console.log("script running",document.querySelector('#formContainer form'))
-document.querySelector('.btn').addEventListener("click",()=>{
-    console.log("submitted")
-// setTimeout(window.location.reload("https://cevi-med-dev.github.io/Call-Log-Form/"), 2000);
-})
-
+var dataObject = {}
 let form = document.querySelector('#formContainer form')
+let postDataa = async (url, data) => {
+    const response = await fetch(url, {
+      method: "POST",
+      cache: "no-cache",
+      mode: 'no-cors',
+      headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: data, // body data type must match "Content-Type" header
+    });
+
+    return response; // parses JSON response into native JavaScript objects
+}
+
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
-    const fn = new FormData(form);
-    let staObject = Object.fromEntries(fn)
-    console.log("this is the data retreived",fn )
-    // for(item of fn){
-    //     console.log(item)
-    // }
+    const dataObject = new FormData(form);
+    // dataObject = Object.fromEntries(fn)
+    console.log("this is the data retreived", dataObject)
+    postDataa("https://hooks.airtable.com/workflows/v1/genericWebhook/appi0FYLXUm0K6RqJ/wflJeWOC1frY12aRs/wtrw525FyOiBa1ZBZ",  dataObject).then((dataa) => {
+        console.log(dataa)
+    });
+ 
 })
+
+
+//send data to Airtable
+
