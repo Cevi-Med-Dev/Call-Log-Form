db8ab461d3390966bdb4e426b35a7a7d7047be52
JamesAChavesForero <javeschames2024@gmail.com>
2024-08-02 09:14:26 -0500

    testing bugfix

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+console.log("script running",document.querySelector('#formContainer form'))
+document.querySelector('#formContainer form').addEventListener("submit",(e)=>{
+    e.preventDefault()
+})
